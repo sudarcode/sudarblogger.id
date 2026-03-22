babf0301f366fc2854400152c5d9f7376fbdf01c
Sudar Blogger <terateinka@gmail.com>
2026-03-22 22:26:18 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ export const SITE = {
   postPerIndex: 6,
   postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
+  showArchives: false,
   showGalleries: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Editar este post",
     url: "https://github.com/0xdres/astro-devosfera/edit/main/",
   },
@@ -22,7 +22,7 @@ export const SITE = {
   lang: "es", // html lang code. Set this empty and default will be "en"
   timezone: "America/Guatemala", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
   introAudio: {
-    enabled: true, // mostrar/ocultar el reproductor en el hero
+    enabled: false, // mostrar/ocultar el reproductor en el hero
     src: "/audio/intro-web.mp3", // ruta al archivo (relativa a /public)
     label: "INTRO.MP3", // etiqueta display en el reproductor
     duration: 30, // duración en segundos (para la barra de progreso fija)
